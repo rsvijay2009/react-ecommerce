36aeda060496588dc9d895d4eb525887d26eec40
Vijaykumar <rsvijay2009@gmail.com>
2025-03-11 21:55:34 +0530

    Initial commit

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import '../styles/Productlist.css';
+import Loading from './Loading';
+
+function ProductList() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+        try {
+            const response = await fetch("https://dummyjson.com/products");
+            const data = await response.json();
+            setProducts(data.products);
+        } catch (error) {
+            console.error('Error fetching products:', error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    fetchProducts();
+  }, []);
+
+  if (loading) return <Loading />
+
+  
+  return (
+    <div className="product-container">
+      {products.map((product) => (
+        <div key={product.id} className="product-card">
+          <img src={product.thumbnail} alt={product.title} className="product-image" />
+          <h3 className="product-title">{product.title}</h3>
+          {/* <p className="product-description">{product.description}</p> */}
+          <p className="product-price">${product.price}</p>
+          <button className="buy-btn">Buy Now</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ProductList
